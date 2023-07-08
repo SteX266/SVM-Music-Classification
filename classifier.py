79bacf4ff5aecfe6c90f963bd561ee490d7e7a17
SteX266 <stefan.milosevic.e14@gmail.com>
2023-07-08 15:57:26 +0200

    Dodat top 2 i top 3 accuracy

--- classifier.py
+++ classifier.py
@@ -27,14 +27,9 @@ y = label_encoder.fit_transform(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Define the parameter grid for grid search
-# TODO: IGRAJ SE SA PARAMETRIMA MAJMUNE
-# TODO: DODAJ MOZDA I NEKE DRUGE KLASIFIKATORE MOZDA JE TO PAMETNO
-# TODO: DODAJ JOS NEKE FEATURE I MOZDA IZBACI NEKE PROBAJ PAR VARIJANTI, TREBAO BI MOZDA PROVERITI STA KOJI FEATURE PREDSTAVLJA
-# TODO: STA RADI SERFEZE, TESTIRA KOD
 param_grid = {
     'kernel': ['rbf'],
     'C': [1, 1, 10],
-    'gamma': [0.00001, 0.4, 3]
 }
 # Create the SVM classifier
 svm = SVC()
@@ -51,6 +46,34 @@ best_params = grid_search.best_params_
 print("Training the SVM...")
 svm_best = SVC(**best_params)
 svm_best.fit(X_train, y_train)
+
+
+decision_scores = svm_best.decision_function(X_test)
+
+print("Decision scores:")
+print(decision_scores)
+
+# Sort the decision scores in descending order for each instance
+top_3_predictions = decision_scores.argsort(axis=1)[:, ::-1][:, :3]
+print("top 3 predictions")
+print(top_3_predictions)
+
+
+# Calculate the top 3 accuracy
+correct_predictions = sum(any(true_label in top_3 for top_3 in top_3_predictions) for true_label in y_test)
+top_3_accuracy = correct_predictions / len(y_test)
+print("Top 3 Accuracy:", top_3_accuracy)
+
+
+top_2_predictions = decision_scores.argsort(axis=1)[:, ::-1][:, :2]
+print("top 2 predictions")
+
+print(top_2_predictions)
+
+# Calculate the top 2 accuracy
+correct_predictions = sum(any(true_label in top_2 for top_2 in top_2_predictions) for true_label in y_test)
+top_2_accuracy = correct_predictions / len(y_test)
+print("Top 2 Accuracy:", top_2_accuracy)
 
 # Evaluate the SVM on the testing set
 accuracy = svm_best.score(X_test, y_test)
